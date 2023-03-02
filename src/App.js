acdd9b5fbe7e0d69fdb6c415d7a8d85f1448d2b6
Artem Vorui <artemvoruy@gmail.com>
2023-03-02 02:01:04 +0200

    remove Navibar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.scss';
-import Navibar from './components/Header/Navbar';
+import Header from './components/Header/Header';
 
 function App() {
   return (
       <>
-        <Navibar/>
+        <Header/>
       </>
   );
 }
