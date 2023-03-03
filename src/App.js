d5317730405a1070b7f6a174e6a0b31536afa76c
Artem Vorui <artemvoruy@gmail.com>
2023-03-03 11:35:22 +0200

    add About

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.scss';
 import Header from './components/Header/Header';
 import Services from './components/Services/Services';
+import About from './components/About/About';
 
 function App() {
   return (
       <>
         <Header/>
         <Services/>
+        <About/>
       </>
   );
 }
