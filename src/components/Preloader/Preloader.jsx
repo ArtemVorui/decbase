f30d7a89b459a3cc68a1720ce6692ca9892b9de8
Artem Vorui <artemvoruy@gmail.com>
2023-03-06 17:24:15 +0200

    add useState, useEffect

--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -1,10 +1,26 @@
 import s from './Preloader.module.scss';
+import {useEffect, useState} from 'react';
 
 const Preloader = () => {
+
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLoading(false);
+        }, 5000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
-        <div className={s.preloader}>
-            <div className={s.preloader__icon}>Loading...</div>
-        </div>
+        <>
+            {loading && (
+                <div className={s.preloader}>
+                    <div className={s.preloader__icon}>Loading...</div>
+                </div>
+            )}
+        </>
     );
 };
 
