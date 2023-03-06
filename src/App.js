07a711a53016a25cae08b52debd4d9ed462f1ff9
Artem Vorui <artemvoruy@gmail.com>
2023-03-06 21:25:39 +0200

    add handleUserActivity

--- src/App.js
+++ src/App.js
@@ -8,19 +8,55 @@ import Blog from './components/Blog/Blog';
 import Contact from './components/Contact/Contacts';
 import Footer from './components/Footer/Footer';
 import Preloader from './components/Preloader/Preloader';
+import {useEffect, useState} from 'react';
 
 function App() {
+
+    const [timer, setTimer] = useState(null);
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            if (window.confirm('Are you still here? Click OK if you want to continue browsing the website.')) {
+                setTimer(null);
+            } else {
+                window.close();
+            }
+        }, 65000);
+
+        setTimer(timeoutId);
+
+        return () => clearTimeout(timeoutId);
+    }, []);
+
+    function handleUserActivity() {
+        if (timer) {
+            clearTimeout(timer);
+        }
+
+        const timeoutId = setTimeout(() => {
+            if (window.confirm('Are you still here? Click OK if you want to continue browsing the website.')) {
+                setTimer(null);
+            } else {
+                window.close();
+            }
+        }, 60000);
+
+        setTimer(timeoutId);
+    }
+
     return (
         <>
             <Preloader/>
-            <Header/>
-            <Services/>
-            <About/>
-            <Statistics/>
-            <Testimonials/>
-            <Blog/>
-            <Contact/>
-            <Footer/>
+            <div onMouseMove={handleUserActivity} onClick={handleUserActivity}>
+                <Header/>
+                <Services/>
+                <About/>
+                <Statistics/>
+                <Testimonials/>
+                <Blog/>
+                <Contact/>
+                <Footer/>
+            </div>
         </>
     );
 }
